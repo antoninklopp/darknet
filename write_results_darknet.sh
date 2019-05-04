9bf06c9ff9c333b34a34ced481335c90f589f8d1
Antonin <antonin.klopp-tosser@grenoble-inp.org>
2019-05-04 21:40:53 +0200

    Added a small option to test only part of dataset

--- write_results_darknet.sh
+++ write_results_darknet.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm output_global.txt
 index=0
+max=1
 for filename in ../DataChallenge/train_individuals/*.jpg; do
     ./darknet detector test cfg/obj.data cfg/tiny_yolo_perso.cfg backup/tiny_yolo_perso_last.weights "$filename" -thresh 0.3 -dont_show
     echo "$index" >> output_global.txt
@@ -9,4 +10,9 @@ for filename in ../DataChallenge/train_individuals/*.jpg; do
         rm output.txt
     fi
     index=$(($index + 1))
-done
+    if [ "$index" -eq "$max" ]; then
+        if [ $1 = "small" ]; then 
+            break
+        fi
+    fi
+done    
