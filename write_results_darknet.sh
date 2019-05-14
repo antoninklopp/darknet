6e97622f38c02857659deb8c307cdad102b39857
Antonin <antonin.klopp-tosser@grenoble-inp.org>
2019-05-14 22:23:41 +0200

    Added a new yolo network

--- write_results_darknet.sh
+++ write_results_darknet.sh
@@ -2,4 +2,4 @@
 rm result.txt
 rm output.txt
 rm output_global.txt
-./darknet detector test cfg/obj.data cfg/tiny_yolo_perso.cfg backup/tiny_yolo_perso_last.weights -dont_show -ext_output -thresh 0.5 < ../train.txt > output_global.txt
+./darknet detector test cfg/obj.data cfg/yolo_perso.cfg backup/tiny_yolo_perso_last.weights -dont_show -ext_output -thresh 0.5 < ../train.txt > output_global.txt
